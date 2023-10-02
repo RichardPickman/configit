337a5c6717837b0a98adbd34003d20863811ddfe
RichardPickman <drezzerock@gmail.com>
2023-10-02 13:51:48 +0200

    rearrange functions

--- src/index.ts
+++ src/index.ts
@@ -1,101 +1,132 @@
-import { input, select } from '@inquirer/prompts';
-import { readFileSync, writeFileSync } from 'fs';
-import path from 'path';
-import { executeCommandWithFileReader, executeCommandWithLogsReader } from './utils/executors';
-import { getOutputFileName, isOutputExist } from './utils/outputs';
+import { input, select } from "@inquirer/prompts";
+import chalk from "chalk";
+import { readFileSync, writeFileSync } from "fs";
+import path from "path";
+import {
+    executeCommandWithFileReader,
+    executeCommandWithLogsReader,
+} from "./utils/executors";
+import { getOutputFileName, isOutputExist } from "./utils/outputs";
 
 const CALL_DIRECTORY = process.cwd();
+const choices = [
+    { name: "yarn", value: "yarn " },
+    { name: "npm", value: "npm run " },
+    { name: "pnpm", value: "pnpm " },
+];
 
-const getPrompts = async () => {
-    const command = await input({ message: 'What is the command?' });
-    const manager = await select({ message: 'Package manager:', choices: [
-        {
-            name: 'yarn',
-            value: 'yarn ',
-        },
-        {
-            name: 'npm',
-            value: 'npm run ',
-        },
-        {
-            name: 'pnpm',
-            value: 'pnpm ',
-        },
-    ], });
+type LogsTypes = "success" | "error" | "warning" | "log";
 
-    console.log('All the necessary prompts recieved! Proceeding with cdk deploy command');
+const { stderr, stdout } = process;
 
-    return {
-        command,
-        manager
-    }
-};
-
-const getScriptString = (command: string) => {
-    const filePath = path.join(CALL_DIRECTORY, 'package.json');
-    const result = JSON.parse(readFileSync(filePath, 'utf-8'));
-    const isScriptExist = Object.keys(result.scripts).includes(command);
-  
-    if (!isScriptExist) {
-        console.log('There is no provided script');
-        process.exit(1);
-    }
-
-    return result.scripts[command];
-}
-
-const callCDK = async (command: string, packageManager: string) => {
-    console.log('Getting script line...');
-
-    const script = getScriptString(command);
-
-    console.log('Script line recieved, proceeding with command: ', script);
-
-    const isOutput = isOutputExist(script);
-    const composedCommand = packageManager + command;
-
-    console.log('Is output present in script line: ', isOutput);
-
-    switch (isOutput) {
-        case true: {
-            const fileName = getOutputFileName(script);
-
-            console.log('Output file exist. Filename is: ', fileName);
-            
-            const result = executeCommandWithFileReader(composedCommand, fileName);
-            
-            return result;
-        }
-        case false: {
-            console.log('Output file is not defined. Proceeding with logs reader');
-            const result = executeCommandWithLogsReader(composedCommand);
-
-            return result;
-        }
-    }
-}
-
-const createEnvironmentFile = (store: string[]) => {
-    const parsedStore = store.join('\n');
-    const envFile = path.join(CALL_DIRECTORY, '.env');
-
-    console.log('Got all key-value pairs, creating .env file');
-
-    writeFileSync(envFile, parsedStore);
-
-    console.log('.env file created');
-    console.log('I am finished here, terminating process...');
-}
-
-const init = async () => {
+async function init() {
     const prompts = await getPrompts();
     const generatedValues = await callCDK(prompts.command, prompts.manager);
 
-    console.log('All values recieved without errors. Final variables: ', generatedValues);
-
     createEnvironmentFile(generatedValues);
 
+    function log(log: string, type: LogsTypes = "log") {
+        const success = (text: string) => stdout.write(chalk.green(text));
+        const warning = (text: string) => stdout.write(chalk.yellow(text));
+        const error = (text: string) => stderr.write(chalk.red(text));
+        const logger = (text: string) => stdout.write(chalk.cyan(text));
+
+        switch (type) {
+            case "success":
+                success(`${log}\n`);
+                break;
+            case "warning":
+                warning(`${log}\n`);
+                break;
+            case "error":
+                error(`${log}\n`);
+                break;
+            case "log":
+                logger(`${log}\n`);
+                break;
+        }
+    }
+
+    async function getPrompts() {
+        const command = await input({ message: "What is the command?" });
+        const manager = await select({ message: "Package manager:", choices });
+
+        log(
+            "All the necessary prompts recieved! Proceeding with cdk deploy command"
+        );
+
+        return {
+            command,
+            manager,
+        };
+    }
+
+    function getScriptString(command: string): string {
+        const filePath = path.join(CALL_DIRECTORY, "package.json");
+        const result = JSON.parse(readFileSync(filePath, "utf-8"));
+        const isScriptExist = Object.keys(result.scripts).includes(command);
+
+        if (!isScriptExist) {
+            log("There is no provided script", "error");
+
+            process.exit(1);
+        }
+
+        return result.scripts[command];
+    }
+
+    async function callCDK(command: string, packageManager: string) {
+        log("Getting script line...");
+
+        const script = getScriptString(command);
+
+        log(
+            `Script line recieved, proceeding with command: ${script}`,
+            "success"
+        );
+
+        const isOutput = isOutputExist(script);
+        const composedCommand = packageManager + command;
+
+        log(`Is output present in script line: ${isOutput}`);
+
+        switch (isOutput) {
+            case true: {
+                const fileName = getOutputFileName(script);
+
+                log(`Output file exist. Filename is: ${fileName}`, "success");
+
+                const result = executeCommandWithFileReader(
+                    composedCommand,
+                    fileName
+                );
+
+                return result;
+            }
+            case false: {
+                log("Output file is not defined. Proceeding with logs reader");
+                const result = await executeCommandWithLogsReader(
+                    composedCommand
+                );
+
+                return result;
+            }
+        }
+    }
+
+    function createEnvironmentFile(store: string[]) {
+        const parsedStore = store.join("\n");
+        const envFile = path.join(CALL_DIRECTORY, ".env");
+
+        log("Got all key-value pairs, creating .env file", "success");
+
+        writeFileSync(envFile, parsedStore);
+
+        log(".env file created", "success");
+        log("I am finished here, terminating process...", "success");
+    }
+
     process.exit(1);
-};
+}
 
 init();
